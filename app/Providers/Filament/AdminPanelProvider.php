908472670695a9429cfd2d9022322bc5ae29f97e
ibrammalik <malikmaul1173@gmail.com>
2025-06-16 23:28:55 +0700

    turn on unsaved changes alert

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->spa()
+            ->unsavedChangesAlerts()
             ->login()
             ->brandName('EsKepal')
             ->colors([
