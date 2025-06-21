f897e97c3caae3f3e4c8657c2d204c2b41690c34
ibrammalik <malikmaul1173@gmail.com>
2025-06-22 00:51:00 +0700

    feat:implement role & permissions spatie

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->foreignId('rw_id')->nullable()->constrained();
             $table->timestamps();
         });
 
