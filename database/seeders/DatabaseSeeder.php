52fb6e2e46a5171564b8a9d5dbe5699b1bbe6c2d
ibrammalik <malikmaul1173@gmail.com>
2025-06-22 00:52:24 +0700

    chore:update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            AgamaSeeder::class,
+            StatusPerkawinanSeeder::class,
+            StatusKependudukanSeeder::class,
+            PekerjaanSeeder::class,
+        ]);
     }
 }
