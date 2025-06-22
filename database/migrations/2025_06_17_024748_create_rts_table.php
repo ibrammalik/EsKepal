4d6556e4c1c6cf6c0420065074accb2d3feb4d83
ibrammalik <malikmaul1173@gmail.com>
2025-06-22 23:23:05 +0700

    refactor: update rts table schema

--- database/migrations/2025_06_17_024748_create_rts_table.php
+++ database/migrations/2025_06_17_024748_create_rts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('rts', function (Blueprint $table) {
             $table->id();
+            $table->string('nomor');
+            $table->foreignId('rw_id')->constrained();
             $table->timestamps();
         });
     }
