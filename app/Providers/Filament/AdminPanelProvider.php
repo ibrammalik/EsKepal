ec7780109494004d081c0826279d04c74eb06143
ibrammalik <malikmaul1173@gmail.com>
2025-07-10 16:04:43 +0700

    feat: install notifications filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -60,6 +60,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->databaseNotifications();
     }
 }
